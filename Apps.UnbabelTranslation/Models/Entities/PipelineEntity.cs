502cfc4251caf32ae18a00bfbe52b1cad9b4e8a0
mathijs-bb <mathijs.sonnemans@blackbird.io>
2023-12-11 11:19:23 +0100

    added delay and updated pipeline displayname

--- Apps.UnbabelTranslation/Models/Entities/PipelineEntity.cs
+++ Apps.UnbabelTranslation/Models/Entities/PipelineEntity.cs
@@ -5,8 +5,11 @@ public class PipelineEntity
     public string Id { get; set; }
     
     public string? Name { get; set; }
+
+    public string? Source_language { get; set; }
+    public string? Target_language { get; set; }
     
     public PipelineGroupEntity Group { get; set; }
 
-    public string DisplayName => Name ?? $"{Id}-{Group.Name}";
+    public string DisplayName => Name ?? $"{Group.Name}: {Source_language} - {Target_language}";
 }
